debea3f0fca827aeb6301ed9cd1a0a696aa54e85
James Tan <jamestan@Jamess-Mac-Studio.local>
2022-10-23 12:32:42 -0700

    Initial Commit

---
+++ pychain.py
@@ -0,0 +1,169 @@
+# Blockchain Ledger
+################################################################################
+# This app is a Blockchain ledger for storing sender, receiver and amount data
+# for transactions. It is developed using the following steps:
+
+# Step 1: Create a Record Data Class that serves as the blueprint for the 
+# financial transaction records that the blocks of the ledger will store.
+
+# Step 2: Create a new data class named `Block` to Store Record Data
+
+# Step 3: Add Relevant User Inputs to the Streamlit Interface
+
+
+################################################################################
+# Imports
+import streamlit as st
+from dataclasses import dataclass
+from typing import List
+import datetime as datetime
+import pandas as pd
+import hashlib
+
+################################################################################
+# Step 1:
+# Create a Record Data Class
+
+# Create a Record Data Class that consists of the `sender`, `receiver`, and
+# `amount` attributes
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
+
+
+################################################################################
+# Step 2: Create a new data class named `Block` to Store Record Data
+
+@dataclass
+class Block:
+
+    record: Record
+
+    creator_id: int
+    prev_hash: str = "0"
+    timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
+    nonce: int = 0
+
+    def hash_block(self):
+        sha = hashlib.sha256()
+
+        record = str(self.record).encode()
+        sha.update(record)
+
+        creator_id = str(self.creator_id).encode()
+        sha.update(creator_id)
+
+        timestamp = str(self.timestamp).encode()
+        sha.update(timestamp)
+
+        prev_hash = str(self.prev_hash).encode()
+        sha.update(prev_hash)
+
+        nonce = str(self.nonce).encode()
+        sha.update(nonce)
+
+        return sha.hexdigest()
+
+@dataclass
+class PyChain:
+    chain: List[Block]
+    difficulty: int = 4
+
+    def proof_of_work(self, block):
+
+        calculated_hash = block.hash_block()
+
+        num_of_zeros = "0" * self.difficulty
+
+        while not calculated_hash.startswith(num_of_zeros):
+
+            block.nonce += 1
+
+            calculated_hash = block.hash_block()
+
+        print("Wining Hash", calculated_hash)
+        return block
+
+    def add_block(self, candidate_block):
+        block = self.proof_of_work(candidate_block)
+        self.chain += [block]
+
+    def is_valid(self):
+        block_hash = self.chain[0].hash_block()
+
+        for block in self.chain[1:]:
+            if block_hash != block.prev_hash:
+                print("Blockchain is invalid!")
+                validity_feedback = "Blockchain is invalid!"
+                return validity_feedback
+
+            block_hash = block.hash_block()
+
+        print("Blockchain is Valid")
+        validity_feedback = "Blockchain is Valid"
+        return validity_feedback
+
+################################################################################
+# Streamlit Code
+
+# Adds the cache decorator for Streamlit
+
+@st.cache(allow_output_mutation=True)
+def setup():
+    print("Initializing Chain")
+    return PyChain([Block("Genesis", 0)])
+
+st.markdown("# Blockchain")
+st.markdown("## Store a Transaction Record in the Blockchain")
+
+pychain = setup()
+
+################################################################################
+# Step 3:
+# Add Relevant User Inputs to the Streamlit Interface
+
+# Add an input area for `sender` from the user.
+sender = st.text_input("Sender")
+
+# Add an input area for `receiver` from the user.
+receiver = st.text_input("Receiver")
+
+# Add an input area for `amount` from the user.
+amount = st.text_input("Amount")
+
+if st.button("Add Block"):
+    prev_block = pychain.chain[-1]
+    prev_block_hash = prev_block.hash_block()
+
+    new_block = Block(
+        record=Record(sender, receiver, amount),
+        creator_id=42,
+        prev_hash=prev_block_hash
+    )
+
+    pychain.add_block(new_block)
+    st.balloons()
+
+################################################################################
+# Streamlit Code (continues)
+
+st.markdown("## The Blockchain Ledger")
+
+pychain_df = pd.DataFrame(pychain.chain).astype(str)
+st.write(pychain_df)
+
+difficulty = st.sidebar.slider("Block Difficulty", 1, 5, 2)
+pychain.difficulty = difficulty
+
+st.sidebar.write("# Block Inspector")
+selected_block = st.sidebar.selectbox(
+    "Which block would you like to see?", pychain.chain
+)
+
+st.sidebar.write(selected_block)
+
+if st.button("Validate Chain"):
+    st.write(pychain.is_valid())
+
